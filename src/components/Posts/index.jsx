a4b928f63433f72c1bdb6b92a0ffffa97eccb7c0
Renato Barros <renato.l@lyncas.net>
2022-08-27 13:41:21 -0300

    ''

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -4,7 +4,7 @@ import './styles.css';
 
 import PostCard from '../PostCard';
 
-const Posts = ({ posts = [] }) => {
+const Posts = ({ posts }) => {
   return (
     <div className="posts">
       {posts.map((post) => (
