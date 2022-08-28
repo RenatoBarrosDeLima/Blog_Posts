a7cce1000693e232fc7f889b1e216bccd50c6d3d
Renato Barros <renato.l@lyncas.net>
2022-08-27 21:13:50 -0300

    -

--- src/components/Posts/Posts.spec.jsx
+++ src/components/Posts/Posts.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 
 import Posts from '.';
 
@@ -7,24 +7,24 @@ import { postsMock } from './mockData';
 const props = postsMock;
 
 describe('<Posts />', () => {
-    it('should render posts', () => {
-        render(<Posts {...props} />);
+  it('should render posts', () => {
+    render(<Posts {...props} />);
 
-        expect(screen.getAllByRole('heading', { name: /title/i })).toHaveLength(3);
-        expect(screen.getAllByRole('img', { name: /title/i })).toHaveLength(3);
-        expect(screen.getAllByText(/body/i)).toHaveLength(3);
-        expect(screen.getByRole('img', { name: /title 3/i })).toHaveAttribute('src', 'img/img3.png');
-    });
+    expect(screen.getAllByRole('heading', { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByRole('img', { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByText(/body/i)).toHaveLength(3);
+    expect(screen.getByRole('img', { name: /title 3/i })).toHaveAttribute('src', 'img/img3.png');
+  });
 
-    it('should not render posts', () => {
-        render(<Posts />);
+  it('should not render posts', () => {
+    render(<Posts />);
 
-        expect(screen.queryByRole('heading', { name: /title/i })).not.toBeInTheDocument();
-    });
+    expect(screen.queryByRole('heading', { name: /title/i })).not.toBeInTheDocument();
+  });
 
-    it('should match snapshot', () => {
-        const { container } = render(<Posts {...props} />);
+  it('should match snapshot', () => {
+    const { container } = render(<Posts {...props} />);
 
-        expect(container.firstChild).toMatchSnapshot();
-    });
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
